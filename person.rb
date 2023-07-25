c6de45ae403e692c0195fd9fe024fb3429ceb6ed
Tamoor saeed <tamoorsaeed22@gmail.com>
2023-07-25 14:12:19 +0500

    Update person.rb

--- person.rb
+++ person.rb
@@ -8,8 +8,11 @@ class Person
     @age = age
     @parent_permission = parent_permission
   end
- def can_use_services?
+
+  def can_use_services?
     of_age? || @parent_permission
+  end
+
   private
 
   def of_age?
