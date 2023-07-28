4e3d83ebeb6e4a6429ea9ef7bfff7a481299f034
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-07-28 12:38:22 +0500

    Create a class Classroom ,@label instance  and  Setter and getter for @Label used  attr_accessor.

---
+++ classroom.rb
@@ -0,0 +1,14 @@
+class Classroom
+    attr_accessor :label
+    attr_reader :students
+
+    def initialize(label)
+        @label = label
+        @students = []
+    end
+
+    def add_student(students)
+        @students.push(students)
+        student.Classroom = self
+    end
+end
