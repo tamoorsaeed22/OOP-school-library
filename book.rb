8367f68fc47d04465f248077e15bffbb1f13bba6
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-08-03 13:24:07 +0500

    Unit tests

--- book.rb
+++ book.rb
@@ -7,8 +7,8 @@ class Book
     @rentals = []
   end
 
-  def add_rentals(rental)
-    @rentals << rental unless @rentals.include?(rental)
+  def add_rental(rental)
+    @rentals.push(rental)
     rental.book = self
   end
 end
